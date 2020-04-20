f580d6af988931d0c22dade9c34143903bcacef5
Boy d'Hont <53119976+boydhont@users.noreply.github.com>
2020-04-20 16:28:52 +0200

    Update a_pathfinding.py

--- src/a_pathfinding.py
+++ src/a_pathfinding.py
@@ -1,20 +1,51 @@
-def getAStarIndices(cells, startIndex, endIndex):
-{
-    closedCells = getClosedCells(cells, startIndex, endIndex)
-    if closedCells == None: return None
-    if len(closedCells) == 0: return None
+class IntegerVector:
+    x = 0
+    y = 0
 
-    AStarIndices = []
-    traceCell = closedCells[len(closedCells)-1]
-    while(traceCell.parentIndex != None):
-        AStarIndices.append(traceCell.index);
-        traceCell = traceCell.getParentCell(closedCells)
-    AStarIndices = reversed(AStarIndices)
-    return AStarIndices
-}
+    def __init__(self, x, y): 
+        self.x = x
+        self.y = y
 
+class AStarCell:
+    index = IntegerVector(0,0)
+    parentIndex = IntegerVector(0,0)
+    g = 0.0
+    h = 0.0
+    f = 0.0
+    
+    def __init__(self, index, parentIndex, startIndex, endIndex): 
+        self.index = index
+        self.parentIndex = parentIndex
+        self.g = self.getIndexDistance(self.index, startIndex)
+        self.h = self.getIndexDistance(self.index, endIndex)
+        self.f = self.g + self.h
+        
+    def getIndexDistance(self, index, targetIndex):
+        return abs(index.x-targetIndex.x) + abs(index.y-targetIndex.y)
+        
+    def isInCellList(self, cellList):
+        for cell in cellList:
+            if self.index.x == cell.index.x and self.index.y == cell.index.y: return True
+        return False
+        
+    def getParentCell(self, cellList):
+        for cell in cellList:
+            if self.parentIndex.x == cell.index.x and self.parentIndex.y == cell.index.y: return cell
+        return None
+    
+    def getAdjacentCells(self, cells, startIndex, endIndex):
+		adjacentCells = []
+		for i in range(-1,2):
+		    for j in range(-1,2):
+		        if i*j != 0: continue
+		        adjacentIndex = IntegerVector(self.index.x + i, self.index.y + j)
+		        if adjacentIndex.x < 0 or adjacentIndex.y < 0 : continue
+		        if adjacentIndex.x >= len(cells) or adjacentIndex >= len(cells[0]): continue
+		        if cells[adjacentCell.x][adjacentCell.y] == None: continue
+		        adjacentCells.append(AStarCell(adjacentindex, self.index, startIndex, endIndex))
+		return adjacentCells
+		
 def getClosedCells(cells, startIndex, endIndex):
-{
     openCells = []
     closedCells = []
 
@@ -35,55 +66,16 @@ def getClosedCells(cells, startIndex, endIndex):
         if len(openCells <= 0): break
 
     return closedCells
-}
-
-class AStarCell:
-{
-    index = IntegerVector(0,0)
-    parentIndex = IntegerVector(0,0)
-    g = 0.0
-    h = 0.0
-    f = 0.0
-
-    def __init__(self, index, parentIndex, startIndex, endIndex): 
-        self.index = index
-        self.parentIndex = parentIndex
-        self.g = self.getIndexDistance(self.index, startIndex)
-        self.h = self.getIndexDistance(self.index, endIndex);
-        self.f = self.g + self.h
-
-    def getIndexDistance(index, targetIndex):
-        return abs(index.x-targetIndex.x) + abs(index.y-targetIndex.y)
     
-    def getAdjacentCells(cells, startIndex, endIndex):
-        adjacentCells = []
-        for i in range(-1,2):
-            for j in range(-1,2):
-                if i*j != 0: continue
-                adjacentIndex = IntegerVector(self.index.x + i, self.index.y + j)
-                if adjacentIndex.x < 0 or adjacentIndex.y < 0 : continue
-                if adjacentIndex.x >= len(cells) or adjacentIndex >= len(cells[0]): continue
-                if cells[adjacentCell.x][adjacentCell.y] == None: continue
-                adjacentCells.append(AStarCell(adjacentindex, self.index, startIndex, endIndex))
-        return adjacentCells
-    
-    def isInCellList(cellList):
-        for cell in cellList:
-            if self.index.x == cell.index.x and self.index.y == cell.index.y: return True
-        return False
-    
-    def getParentCell(cellList):
-        for cell in cellList:
-            if self.parentIndex.x == cell.index.x and self.parentIndex.y == cell.index.y: return cell
-        return None  
-}
+def getAStarIndices(cells, startIndex, endIndex):
+    closedCells = getClosedCells(cells, startIndex, endIndex)
+    if closedCells == None: return None
+    if len(closedCells) == 0: return None
 
-class IntegerVector:
-{
-    x = 0
-    y = 0
-
-    def __init__(self, x, y): 
-        self.x = x
-        self.y = y
-}
+    AStarIndices = []
+    traceCell = closedCells[len(closedCells)-1]
+    while(traceCell.parentIndex != None):
+        AStarIndices.append(traceCell.index)
+        traceCell = traceCell.getParentCell(closedCells)
+    AStarIndices = reversed(AStarIndices)
+    return AStarIndices
