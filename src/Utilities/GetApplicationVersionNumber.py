3d371609697e966e82110d7e6166c038d6965c8d
Boy d'Hont <53119976+boydhont@users.noreply.github.com>
2022-07-29 16:35:00 +0200

    Add files via upload

---
+++ src/Utilities/GetApplicationVersionNumber.py
@@ -0,0 +1,10 @@
+import clr
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import *
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)
+
+OUT = DocumentManager.Instance.CurrentDBDocument.Application.VersionNumber
